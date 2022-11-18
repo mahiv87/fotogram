887862c507b0700ce2614211fd441ebb33e82bde
Marcus Herrera <maherreraiv1987@gmail.com>
2022-11-17 21:51:56 -0700

    apollo client and graphql dependencies added to frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Photo from './components/Photo/Photo';
 import Profile from './screens/Profile/Profile';
+
+// apollo client
+const client = new ApolloClient({
+	uri: 'http://localhost:1337/graphql',
+	cache: new InMemoryCache()
+});
 
 function App() {
 	return (
@@ -28,11 +35,13 @@ function App() {
 		// 		/>
 		// 	</Routes>
 		// </Router>
-		<div className="App">
-			{/* <NavBar /> */}
-			{/* <Photo /> */}
-			<Profile />
-		</div>
+		<ApolloProvider client={client}>
+			<div className="App">
+				{/* <NavBar /> */}
+				{/* <Photo /> */}
+				<Profile />
+			</div>
+		</ApolloProvider>
 	);
 }
 
